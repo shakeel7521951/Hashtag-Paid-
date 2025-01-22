624510408126010be963a498494ef9b406e992b2
Mogehs <mugheesurrehman06@gmail.com>
2025-01-22 16:29:09 +0500

    marque tag in creators

--- src/pages/Creators.jsx
+++ src/pages/Creators.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Hero from '../components/creators/Hero'
+import React from "react";
+import Hero from "../components/creators/Hero";
+import Marque from "../components/creators/Marque";
 
 const Creators = () => {
   return (
     <div>
       <Hero />
+      <Marque />
     </div>
-  )
-}
+  );
+};
 
-export default Creators
+export default Creators;
