8c776b336836311f1cbc8774ba938513dbaadd7f
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-01-23 15:52:24 +0500

    add creators component

--- src/pages/CreatorMarketing.jsx
+++ src/pages/CreatorMarketing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/creatorMarketing/Hero'
+import Creators from '../components/creatorMarketing/Creators'
 
 const CreatorMarketing = () => {
   return (
     <div>
         <Hero />
+        <Creators />
     </div>
   )
 }
