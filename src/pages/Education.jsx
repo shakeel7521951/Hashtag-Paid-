63a73d5e9419c5172e920bff5357b908c4928067
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-01-23 11:48:41 +0500

    add education page , set navbar

---
+++ src/pages/Education.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/education/Hero'
+
+const Education = () => {
+  return (
+    <div>
+        <Hero />
+    </div>
+  )
+}
+
+export default Education
