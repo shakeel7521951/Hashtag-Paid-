1e59861567baf3168c5b4aebda9fe3e84966e261
Mogehs <mugheesurrehman06@gmail.com>
2025-01-25 15:47:28 +0500

    created login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className="w-full h-screen flex justify-center">
+      <div className="h-full w-full lg:w-1/3 pt-40 flex flex-col items-center">
+        <p className="pl-8 text-[2.4rem] lg:w-[80%]">Login To Your Account</p>
+        <input
+          type="text"
+          placeholder="Email"
+          name=""
+          id=""
+          className="ml-8 w-[50%] lg:w-[80%] h-[2.5rem] mt-4 p-2 rounded-[4px] border border-gray-400"
+        />
+        <button className="mt-4 bg-[#ee2a57] w-[50%] lg:w-[80%] ml-8 p-3 rounded-[4px] text-white">
+          Continue
+        </button>
+        <span className="ml-8 mt-2 text-gray-600">
+          Don't have an account yet?{" "}
+          <a href="" className="underline">
+            Signup Here
+          </a>
+        </span>
+      </div>
+      <div className="h-full w-2/3 hidden lg:inline ">
+        <img src="/login.png" alt="" className="object-cover h-full" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
